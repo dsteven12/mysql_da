2a8929786b1ef6c7bcbf8a74e68c858097805058
Darryl Lardizabal <dsteven12@gmail.com>
2018-10-17 15:33:26 -0700

    Finished delete statements section.

--- Delete Statements/delete_practice1.sql
+++ Delete Statements/delete_practice1.sql
@@ -8,3 +8,8 @@ SELECT * FROM titles WHERE emp_no = 999903;
 DELETE FROM employees WHERE emp_no = 999903;
 
 ROLLBACK;
+
+-- DELETE EXERCISES
+DELETE FROM departments WHERE dept_no = 'd010';
+
+SELECT * FROM departments ORDER BY dept_no;
